76f190b8a618d296b0c6aa3fe80f3b405819f498
mikasa-fy <542864279@qq.com>
2023-10-02 14:29:45 +0200

    modify the contents.

--- test.py
+++
@@ -1,6 +0,0 @@
-class Example:
-    def instance_method(self):
-        return "This is an instance method."
-
-obj = Example()
-print(Example().instance_method())  # Output: This is an instance method.
